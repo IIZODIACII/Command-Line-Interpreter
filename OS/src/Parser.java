8bab3138d95fe39dd192f3d8424f267214712fce
yabani98 <31174601+yabani98@users.noreply.github.com>
2018-10-27 17:18:23 +0200

    Update Parser.java

--- OS/src/Parser.java
+++ OS/src/Parser.java
@@ -3,7 +3,8 @@ import java.util.Vector;
 
 public class Parser {
 public Vector<String>cmd;
-public Vector<Vector<String> >args;	
+public Vector<Vector<String> >args;
+public Vector<String>red;
 public boolean Check(String cmd){//to check whether the current command is valid or not
 String []commands={"clear","cd","ls","cp","mv","rm","mkdir","rmdir","cat","more","pwd","exit"};//didn't complete the commands yet :)	
 	for(String i:commands)if(i.equals(cmd))return true;
@@ -13,14 +14,14 @@ return false;
 public boolean parse(String Input){
 cmd=new Vector<String>();
 args=new Vector<Vector<String>>();
+red=new Vector<String>();
 String []sp1=Input.split("\\|");//the commands will be given like cd (1 arg)|pwd(no args)|cp (2 args)
 // so sp1 has every command with it's args
 // and sp2 has splitted the command and arguments
 	for(int i=0;i<sp1.length;i++){
 
 		String []sp2=sp1[i].split(" ");		
-        if(!Check(sp2[0]))return false;//if the given command is invalid then fuck the User
-
+        if(!Check(sp2[0]))return false;//if the given command is invalid 
         Vector<String>v=new Vector<String>();		
 /* we need to keep cmd and args synchornized like
  cd arg
@@ -30,10 +31,13 @@ String []sp1=Input.split("\\|");//the commands will be given like cd (1 arg)|pwd
  */
         cmd.addElement(sp2[0]);
 
-        for(int j=1;j<sp2.length;j++)v.addElement(sp2[i]);
-
+        for(int j=1;j<sp2.length;j++)
+        if(sp2[j].equals(">")||sp2[j].equals(">>")){red.addElement(sp2[j+1]);break;}
+        else v.addElement(sp2[i]);
+        
         if(v.size()==0)v.addElement("");
-
+        if(red.size()==0)red.addElement("");
+        
         args.addElement(v);
 
 	}
